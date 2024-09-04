2e73ed90ab9253b12fac2b9c13ee3e47112ec51f
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-09-04 15:35:30 -0700

    added behavioral events

--- src/schneider_lab_to_nwb/schneider_2024/schneider_2024_behaviorinterface.py
+++ src/schneider_lab_to_nwb/schneider_2024/schneider_2024_behaviorinterface.py
@@ -3,10 +3,13 @@ from pynwb.file import NWBFile
 from pydantic import FilePath
 import numpy as np
 from h5py import File
+from hdmf.common.table import DynamicTableRegion
 from pynwb.behavior import BehavioralTimeSeries, TimeSeries
+from ndx_events import EventTypesTable, EventsTable, Task, TimestampVectorData
 
 from neuroconv.basedatainterface import BaseDataInterface
 from neuroconv.utils import DeepDict
+from neuroconv.tools import nwb_helpers
 
 
 class Schneider2024BehaviorInterface(BaseDataInterface):
@@ -57,4 +60,52 @@ class Schneider2024BehaviorInterface(BaseDataInterface):
             time_series=[encoder_time_series, lick_time_series],
             name="behavioral_time_series",
         )
-        nwbfile.add_acquisition(behavioral_time_series)
+        behavior_module = nwb_helpers.get_module(
+            nwbfile=nwbfile,
+            name="behavior",
+            description="Behavioral data from the experiment.",
+        )
+        behavior_module.add(behavioral_time_series)
+
+        event_types_table = EventTypesTable(name="event_types", description="Metadata about event types.")
+        event_types_table.add_row(
+            event_name="target", event_type_description="Time at which the target zone is entered during a press."
+        )
+        event_types_table.add_row(
+            event_name="target_out", event_type_description="Time at which the target zone is overshot during a press."
+        )
+        event_types_table.add_row(
+            event_name="tone_in", event_type_description="Time at which target entry tone is played."
+        )
+        event_types_table.add_row(
+            event_name="tone_out", event_type_description="Time at which target exit tone is played."
+        )
+        event_types_table.add_row(
+            event_name="tuning_tone",
+            event_type_description="Times at which tuning tones are played to an animal after a behavioral experiment during ephys recording sessions.",
+        )
+        event_types_table.add_row(
+            event_name="valve",
+            event_type_description="Times at which solenoid valve opens to deliver water after a correct trial.",
+        )
+
+        events_table = EventsTable(
+            name="events_table",
+            description="Metadata about events.",
+            target_tables={"event_type": event_types_table},
+        )
+        nested_event_times = [
+            target_times,
+            target_out_times,
+            tone_in_times,
+            tone_out_times,
+            tuning_tone_times,
+            valve_times,
+        ]
+        for i, event_times in enumerate(nested_event_times):
+            for event_time in event_times:
+                events_table.add_row(timestamp=event_time, event_type=i)
+        behavior_module.add(events_table)
+
+        task = Task(event_types=event_types_table)
+        nwbfile.add_lab_meta_data(task)
