9ed01ed9073c062e326022a36ddd574301f800e2
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-10-04 13:56:48 -0700

    moved metadata to yaml

--- src/schneider_lab_to_nwb/schneider_2024/schneider_2024_intrinsic_signal_imaging_interface.py
+++ src/schneider_lab_to_nwb/schneider_2024/schneider_2024_intrinsic_signal_imaging_interface.py
@@ -27,6 +27,33 @@ class Schneider2024IntrinsicSignalOpticalImagingInterface(BaseDataInterface):
 
     def get_metadata_schema(self) -> dict:
         metadata_schema = super().get_metadata_schema()
+        metadata_schema["properties"]["IntrinsicSignalOpticalImaging"] = dict()
+        metadata_schema["properties"]["IntrinsicSignalOpticalImaging"]["properties"] = {
+            "Module": {
+                "properties": {
+                    "name": {"type": "string"},
+                    "description": {"type": "string"},
+                },
+            },
+            "Images": {
+                "properties": {
+                    "name": {"type": "string"},
+                    "description": {"type": "string"},
+                },
+            },
+            "RawImage": {
+                "properties": {
+                    "name": {"type": "string"},
+                    "description": {"type": "string"},
+                },
+            },
+            "ProcessedImage": {
+                "properties": {
+                    "name": {"type": "string"},
+                    "description": {"type": "string"},
+                },
+            },
+        }
         return metadata_schema
 
     def add_to_nwbfile(self, nwbfile: NWBFile, metadata: dict):
@@ -40,24 +67,25 @@ class Schneider2024IntrinsicSignalOpticalImagingInterface(BaseDataInterface):
             processed_image_array = np.array(image)
 
         # Add Data to NWBFile
+        isoi_metadata = metadata["IntrinsicSignalOpticalImaging"]
         isoi_module = nwb_helpers.get_module(
             nwbfile=nwbfile,
-            name="intrinsic_signal_optical_imaging",
-            description="For precise targeting of auditory cortex, intrinsic optical imaging (IOS) will be performed using a 2-photon microscope (Neurolabware). The skull is first bilaterally thinned over a region of interest (ROI) and made translucent. On experiment day, 680nm red light (ThorLabs) is used to image the ROI. Data is collected via MATLAB running custom suites for online and offline analyses.",
+            name=isoi_metadata["Module"]["name"],
+            description=isoi_metadata["Module"]["description"],
         )
         raw_image = GrayscaleImage(
-            name="raw_image",
+            name=isoi_metadata["RawImage"]["name"],
             data=raw_image_array,
-            description="Original image capture of ROI for intrinsic imaging.",
+            description=isoi_metadata["RawImage"]["description"],
         )
         processed_image = RGBImage(
-            name="processed_image",
+            name=isoi_metadata["ProcessedImage"]["name"],
             data=processed_image_array,
-            description="Original image capture of ROI overlaid with topographic map of processed intrinsic signal.",
+            description=isoi_metadata["ProcessedImage"]["description"],
         )
         images = Images(
-            name="images",
-            description="Intrinsic signal optical images.",
+            name=isoi_metadata["Images"]["name"],
+            description=isoi_metadata["Images"]["description"],
             images=[raw_image, processed_image],
         )
         isoi_module.add(images)
