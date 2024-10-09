c1bc18ff243fc5338e9cb7b1d6cb0ca5bfa2adff
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-10-09 08:53:18 -0700

    Union[str, Path] --> str | Path

--- src/schneider_lab_to_nwb/schneider_2024/schneider_2024_convert_all_sessions.py
+++ src/schneider_lab_to_nwb/schneider_2024/schneider_2024_convert_all_sessions.py
@@ -1,6 +1,5 @@
 """Primary script to run to convert all sessions in a dataset using session_to_nwb."""
 from pathlib import Path
-from typing import Union
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from pprint import pformat
 import traceback
@@ -11,8 +10,8 @@ from .schneider_2024_convert_session import session_to_nwb
 
 def dataset_to_nwb(
     *,
-    data_dir_path: Union[str, Path],
-    output_dir_path: Union[str, Path],
+    data_dir_path: str | Path,
+    output_dir_path: str | Path,
     max_workers: int = 1,
     verbose: bool = True,
 ):
@@ -20,9 +19,9 @@ def dataset_to_nwb(
 
     Parameters
     ----------
-    data_dir_path : Union[str, Path]
+    data_dir_path : str | Path
         The path to the directory containing the raw data.
-    output_dir_path : Union[str, Path]
+    output_dir_path : str | Path
         The path to the directory where the NWB files will be saved.
     max_workers : int, optional
         The number of workers to use for parallel processing, by default 1
@@ -39,7 +38,7 @@ def dataset_to_nwb(
         for session_to_nwb_kwargs in session_to_nwb_kwargs_per_session:
             session_to_nwb_kwargs["output_dir_path"] = output_dir_path
             session_to_nwb_kwargs["verbose"] = verbose
-            exception_file_path = data_dir_path / f"ERROR_<nwbfile_name>.txt" # Add error file path here
+            exception_file_path = data_dir_path / f"ERROR_<nwbfile_name>.txt"  # Add error file path here
             futures.append(
                 executor.submit(
                     safe_session_to_nwb,
@@ -51,7 +50,7 @@ def dataset_to_nwb(
             pass
 
 
-def safe_session_to_nwb(*, session_to_nwb_kwargs: dict, exception_file_path: Union[Path, str]):
+def safe_session_to_nwb(*, session_to_nwb_kwargs: dict, exception_file_path: str | Path):
     """Convert a session to NWB while handling any errors by recording error messages to the exception_file_path.
 
     Parameters
@@ -72,13 +71,13 @@ def safe_session_to_nwb(*, session_to_nwb_kwargs: dict, exception_file_path: Uni
 
 def get_session_to_nwb_kwargs_per_session(
     *,
-    data_dir_path: Union[str, Path],
+    data_dir_path: str | Path,
 ):
     """Get the kwargs for session_to_nwb for each session in the dataset.
 
     Parameters
     ----------
-    data_dir_path : Union[str, Path]
+    data_dir_path : str | Path
         The path to the directory containing the raw data.
 
     Returns
@@ -86,11 +85,11 @@ def get_session_to_nwb_kwargs_per_session(
     list[dict[str, Any]]
         A list of dictionaries containing the kwargs for session_to_nwb for each session.
     """
-     #####
-     # # Implement this function to return the kwargs for session_to_nwb for each session
-     # This can be a specific list with hard-coded sessions, a path expansion or any conversion specific logic that you might need
-     #####
-    raise NotImplementedError 
+    #####
+    # # Implement this function to return the kwargs for session_to_nwb for each session
+    # This can be a specific list with hard-coded sessions, a path expansion or any conversion specific logic that you might need
+    #####
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
