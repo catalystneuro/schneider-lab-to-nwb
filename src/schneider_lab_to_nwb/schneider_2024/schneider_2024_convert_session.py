4dbeb5381d193c2d86446aed0291f40a855f0730
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-11-18 12:55:29 -0800

    refactored for optional opto and optional ephys

--- src/schneider_lab_to_nwb/schneider_2024/schneider_2024_convert_session.py
+++ src/schneider_lab_to_nwb/schneider_2024/schneider_2024_convert_session.py
@@ -4,22 +4,21 @@ from zoneinfo import ZoneInfo
 import shutil
 import numpy as np
 from datetime import datetime
+from typing import Optional
 
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 from schneider_lab_to_nwb.schneider_2024 import Schneider2024NWBConverter
 
 
 def session_to_nwb(
-    recording_folder_path: str | Path,
-    sorting_folder_path: str | Path,
     behavior_file_path: str | Path,
     video_folder_path: str | Path,
     intrinsic_signal_optical_imaging_folder_path: str | Path,
     output_dir_path: str | Path,
+    ephys_folder_path: Optional[str | Path] = None,
+    has_opto: bool = False,
     stub_test: bool = False,
 ):
-    recording_folder_path = Path(recording_folder_path)
-    sorting_folder_path = Path(sorting_folder_path)
     behavior_file_path = Path(behavior_file_path)
     video_folder_path = Path(video_folder_path)
     intrinsic_signal_optical_imaging_folder_path = Path(intrinsic_signal_optical_imaging_folder_path)
@@ -31,6 +30,11 @@ def session_to_nwb(
     if stub_test:
         output_dir_path = output_dir_path / "nwb_stub"
     output_dir_path.mkdir(parents=True, exist_ok=True)
+    if ephys_folder_path is None:
+        has_ephys = False
+    else:
+        has_ephys = True
+        ephys_folder_path = Path(ephys_folder_path)
 
     session_id = "sample_session"
     nwbfile_path = output_dir_path / f"{session_id}.nwb"
@@ -38,14 +42,14 @@ def session_to_nwb(
     source_data = dict()
     conversion_options = dict()
 
-    # Add Recording
-    stream_name = "Signals CH"
-    source_data.update(dict(Recording=dict(folder_path=recording_folder_path, stream_name=stream_name)))
-    conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
+    # Add Ephys Recording and Sorting
+    if has_ephys:
+        stream_name = "Signals CH"
+        source_data.update(dict(Recording=dict(folder_path=ephys_folder_path, stream_name=stream_name)))
+        conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
 
-    # Add Sorting
-    source_data.update(dict(Sorting=dict(folder_path=sorting_folder_path)))
-    conversion_options.update(dict(Sorting=dict()))
+        source_data.update(dict(Sorting=dict(folder_path=ephys_folder_path)))
+        conversion_options.update(dict(Sorting=dict()))
 
     # Add Behavior
     source_data.update(dict(Behavior=dict(file_path=behavior_file_path)))
@@ -58,8 +62,9 @@ def session_to_nwb(
     #     conversion_options.update({metadata_key_name: dict()})
 
     # Add Optogenetic
-    # source_data.update(dict(Optogenetic=dict(file_path=behavior_file_path)))
-    # conversion_options.update(dict(Optogenetic=dict()))
+    if has_opto:
+        source_data.update(dict(Optogenetic=dict(file_path=behavior_file_path)))
+        conversion_options.update(dict(Optogenetic=dict()))
 
     # Add Intrinsic Signal Optical Imaging
     # source_data.update(dict(ISOI=dict(folder_path=intrinsic_signal_optical_imaging_folder_path)))
@@ -87,21 +92,22 @@ def session_to_nwb(
     conversion_options["Sorting"]["units_description"] = metadata["Sorting"]["units_description"]
 
     # Add electrode metadata
-    channel_positions = np.load(sorting_folder_path / "channel_positions.npy")
-    if True:  # stub_test: SWITCH BACK TO stub_test WHEN ALL CHANNELS ARE PRESENT
-        channel_positions = channel_positions[:1, :]
-    location = metadata["Ecephys"]["ElectrodeGroup"][0]["location"]
-    channel_ids = converter.data_interface_objects["Recording"].recording_extractor.get_channel_ids()
-    converter.data_interface_objects["Recording"].recording_extractor.set_channel_locations(
-        channel_ids=channel_ids, locations=channel_positions
-    )
-    converter.data_interface_objects["Recording"].recording_extractor.set_property(
-        key="brain_area",
-        ids=channel_ids,
-        values=[location] * len(channel_ids),
-    )
-    converter.data_interface_objects["Recording"].recording_extractor._recording_segments[0].t_start = 0.0
-    metadata["Ecephys"]["Device"] = editable_metadata["Ecephys"]["Device"]
+    if has_ephys:
+        channel_positions = np.load(ephys_folder_path / "channel_positions.npy")
+        if True:  # stub_test: SWITCH BACK TO stub_test WHEN ALL CHANNELS ARE PRESENT
+            channel_positions = channel_positions[:1, :]
+        location = metadata["Ecephys"]["ElectrodeGroup"][0]["location"]
+        channel_ids = converter.data_interface_objects["Recording"].recording_extractor.get_channel_ids()
+        converter.data_interface_objects["Recording"].recording_extractor.set_channel_locations(
+            channel_ids=channel_ids, locations=channel_positions
+        )
+        converter.data_interface_objects["Recording"].recording_extractor.set_property(
+            key="brain_area",
+            ids=channel_ids,
+            values=[location] * len(channel_ids),
+        )
+        converter.data_interface_objects["Recording"].recording_extractor._recording_segments[0].t_start = 0.0
+        metadata["Ecephys"]["Device"] = editable_metadata["Ecephys"]["Device"]
 
     # # Overwrite video metadata
     # for i, video_file_path in enumerate(video_file_paths):
@@ -127,20 +133,31 @@ def main():
         shutil.rmtree(output_dir_path, ignore_errors=True)
 
     # Example Session A1 Ephys + Behavior
-    recording_folder_path = data_dir_path / "A1_EphysFiles" / "m53" / "Day1_A1"
-    sorting_folder_path = recording_folder_path
+    ephys_folder_path = data_dir_path / "A1_EphysFiles" / "m53" / "Day1_A1"
     behavior_file_path = data_dir_path / "A1_EphysBehavioralFiles" / "raw_m53_231029_001.mat"
     video_folder_path = Path("")
     intrinsic_signal_optical_imaging_folder_path = Path("")
     session_to_nwb(
-        recording_folder_path=recording_folder_path,
-        sorting_folder_path=sorting_folder_path,
+        ephys_folder_path=ephys_folder_path,
         behavior_file_path=behavior_file_path,
         video_folder_path=video_folder_path,
         intrinsic_signal_optical_imaging_folder_path=intrinsic_signal_optical_imaging_folder_path,
         output_dir_path=output_dir_path,
         stub_test=stub_test,
     )
+
+    # # Example Session A1 Ogen + Behavior
+    # behavior_file_path = data_dir_path / "A1_OptoBehavioralFiles" / "raw_m53_231013_001.mat"
+    # video_folder_path = Path("")
+    # intrinsic_signal_optical_imaging_folder_path = Path("")
+    # session_to_nwb(
+    #     ephys_folder_path=recording_folder_path,
+    #     behavior_file_path=behavior_file_path,
+    #     video_folder_path=video_folder_path,
+    #     intrinsic_signal_optical_imaging_folder_path=intrinsic_signal_optical_imaging_folder_path,
+    #     output_dir_path=output_dir_path,
+    #     stub_test=stub_test,
+    # )
 
 
 if __name__ == "__main__":
