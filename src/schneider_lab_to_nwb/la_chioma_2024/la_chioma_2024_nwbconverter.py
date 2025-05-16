7f35b08c7e7acef8e2e6d41cde9dbea7c5459ca0
weiglszonja <wszonja@gmail.com>
2025-05-16 11:35:46 +0200

    Add behavior interface to NWBConverter for La Chioma 2024

--- src/schneider_lab_to_nwb/la_chioma_2024/la_chioma_2024_nwbconverter.py
+++ src/schneider_lab_to_nwb/la_chioma_2024/la_chioma_2024_nwbconverter.py
@@ -2,10 +2,13 @@
 from neuroconv import NWBConverter
 from neuroconv.datainterfaces import OpenEphysBinaryRecordingInterface
 
+from schneider_lab_to_nwb.la_chioma_2024.la_chioma_2024_behaviorinterface import LaChioma2024BehaviorInterface
+
 
 class LaChioma2024NWBConverter(NWBConverter):
     """Primary conversion class."""
 
     data_interface_classes = dict(
         Recording=OpenEphysBinaryRecordingInterface,
+        Behavior=LaChioma2024BehaviorInterface,
     )
